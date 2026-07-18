c274355fb4811ffb99ebc492ca4093f2ba72b037
Gaurav Nayal <nayalg2003@gmail.com>
2026-07-18 10:43:12 +0530

    react

--- practicereact/src/App.jsx
+++ practicereact/src/App.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-
+console.log("hello");
 
 function App() {
 
-  const[count,setCount] = useState(0);
-  const[countt,setCountt] = useState(100);
+  // const[count,setCount] = useState(0);
 
-  useEffect(()=>{
-    console.log("Element renders");
-    console.log("Button clicked");
-  },[count,countt]);
+  // const add = ()=>{
+  //   console.log("function created");
+  // }
+  // const[countt,setCountt] = useState(100);
+
+  // useEffect(()=>{
+  //   console.log("Element renders");
+  //   console.log("Button clicked");
+  // },[count]);
 
   return (
-   <>
-   <h1>{count}</h1>
-   <button onClick={()=>setCount(count + 1)}>Click</button>
-   <h1>{countt}</h1>
-   <button onClick={()=>setCountt(countt+2)}>Double</button>
-   </>
+  //  <div
+  //  style={{
+  //       backgroundColor: "black",
+  //       color: "white",
+  //       minHeight: "100vh",
+  //       display: "flex",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //       flexDirection: "column",
+  //     }}
+  //  >
+  //  <h1>{count}</h1>
+  //  <button onClick={()=>setCount(count + 1)}>Click</button>
+  //  {/* <h1>{countt}</h1>
+  //  <button onClick={()=>setCountt(countt+2)}>Double</button> */}
+  //  </div>
+  <>
+  </>
   )
 }
 
